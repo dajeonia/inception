b2cf41a4b95ba07b94db30c0cf1b839fef64e4db
Ubuntu <ubuntu@ip-172-31-8-247.ap-northeast-2.compute.internal>
2024-09-22 14:04:10 +0000

    dockerfile: mariadb

---
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+set -e
+
+service mariadb start;
+
+mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+mysql -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
+mysql -e "FLUSH PRIVILEGES;"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+
+mysqladmin -uroot -p$MYSQL_ROOT_PASSWORD shutdown
+
+exec "$@"
